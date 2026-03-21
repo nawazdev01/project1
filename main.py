e7ab00ffa73d5ff2f6291ec39e33971d493987d2
nawazdev01 <nawazdev01@gmail.com>
2026-03-21 16:00:32 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-def get_todos(filepath):
-    
-    with open(filepath,"r") as file_local:
-       todos_local = file_local.readlines()
-    return todos_local
-
-def write_todos(filepath,todos_arg):
-    with open(filepath,"w") as file_local:
-        file_local.writelines(todos_arg)
-
+import functions
+import time
+now = time.strftime("%b-%d-%Y, %H:%M:%S")
+print("It is",now)
 
 while True:
     user_action = input("type add, show, edit, complete or exit: ")
@@ -19,17 +13,17 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos("todos.txt")
+        todos = functions.get_todos("todos.txt")
         todos.append(todo +"\n")
 
-        write_todos("todos.txt",todos)
+        functions.write_todos("todos.txt",todos)
 
 
     elif user_action.startswith("show"):
 
 
 
-        todos = get_todos("todos.txt")
+        todos = functions.get_todos("todos.txt")
 
         for index, item in enumerate(todos):
 
@@ -44,12 +38,12 @@ while True:
             number = int(user_action[5:])
             number = number-1
 
-            todos = get_todos("todos.txt")
+            todos = functions.get_todos("todos.txt")
 
             new_todos= input("Enter new todo: ")
             todos[number] = new_todos + "\n"
 
-            write_todos("todos.txt",todos)
+            functions.write_todos("todos.txt",todos)
 
         except ValueError:
             print("Your command is not valid")
@@ -62,9 +56,9 @@ while True:
         try:
 
             number = int(user_action[9:])
-            todos = get_todos("todos.txt")
+            todos = functions.get_todos("todos.txt")
             todos.pop(number-1)
-            write_todos("todos.txt",todos)
+            functions.write_todos("todos.txt",todos)
 
         except IndexError:
 
