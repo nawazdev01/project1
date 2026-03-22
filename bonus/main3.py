d8ac3e9c2de45c8fdc4416135f99929c42c047eb
nawazdev01 <nawazdev01@gmail.com>
2026-03-22 16:07:47 -0400

    Create basic GUI without interaction

--- bonus/main3.py
+++ bonus/main3.py
@@ -7,17 +7,17 @@ while True:
 
         case "add":
             todo = input("Enter a todo:") +'\n'
-            with open("todos3.txt","r") as file:
+            with open("todos3.txt", "r") as file:
                 todos = file.readlines()
 
             todos.append(todo)
 
-            with open("todos3.txt","w") as file:
+            with open("todos3.txt", "w") as file:
                 file.writelines(todos)
 
 
         case "show":
-            with open("todos3.txt","r") as file:
+            with open("todos3.txt", "r") as file:
                 todos = file.readlines()
 
             for i,j in enumerate(todos):
@@ -26,20 +26,20 @@ while True:
         case "edit":
             number = int(input("Enter the todo number: "))
             number = number - 1
-            with open("todos3.txt","r") as file:
+            with open("todos3.txt", "r") as file:
                 todos = file.readlines()
 
             todos[number] = input("enter the new todo: ") + '\n'
-            with open("todos3.txt","w") as file:
+            with open("todos3.txt", "w") as file:
                 todos = file.writelines(todos)
 
         case "complete":
             number  = int(input("enter the completed todo number: "))
             number = number - 1
-            with open("todos3.txt","r") as file:
+            with open("todos3.txt", "r") as file:
                 todos = file.readlines()
                 todos.pop(number)
-            with open("todos3.txt","w") as file:
+            with open("todos3.txt", "w") as file:
                 file.writelines(todos)
 
         case "exit":
