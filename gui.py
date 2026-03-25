c37219b1be50c7ec7111ebd7402700ea6a652685
nawazdev01 <nawazdev01@gmail.com>
2026-03-25 10:49:08 -0400

    Implemented the Commit button and Exit button

--- gui.py
+++ gui.py
@@ -10,8 +10,12 @@ listbox = sg.Listbox(values = functions.get_todos(),key="todos",
                      enable_events = True, size= [45,10])
 edit_button = sg.Button("Edit")
 
+complete_button = sg.Button("Complete")
+
+exit_button = sg.Button("Exit")
+
 window = sg.Window('My To_Do App',
-                   layout=[[label],[input_box,add_button],[listbox,edit_button]],
+                   layout=[[label],[input_box,add_button],[listbox,edit_button,complete_button],[exit_button]],
                    font =('Helvetica','20'))
 
 while True:
@@ -39,8 +43,19 @@ while True:
         case "todos":
             window['todo'].update(value = values['todos'][0])
 
+        case "Complete":
+            todos = functions.get_todos()
+            todo_to_complete= values['todos'][0]
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values= todos)
+            window['todo'].update(value = '')
+
+
 
         case sg.WIN_CLOSED:
+            break
+        case "Exit":
             break
 
 window.close()
